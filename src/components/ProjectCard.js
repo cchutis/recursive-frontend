4de50f9d3f18e3a0e3102babc8043327fc0c7948
Constantine Chutis <constantinec84@gmail.com>
2019-07-27 15:25:48 -0400

    Hardcoded host in image url to get images working

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import Moment from 'moment'
+// import Moment from 'moment'
 
 export default function ProjectCard (props) {
 
-    const {name, language, due_date, id} = props.project
-
+    const {name, card_photo, language, due_date, id} = props.project
 
     return (
         <div className="project-card">
+            <img src={`http://localhost:4000/${card_photo}`} alt=""/>
             <h2>{name}</h2>
             <h3>{language}</h3>
-            <p>Deadline: {Moment().format('due_date, MMMM-DD-YYYY')}</p>
+            <p>Deadline: {due_date}</p>
             <button onClick={() => props.viewProject(id)}>View Project</button>
         </div>
     )
